bda330a72757892f77c56f2fd3380a29392256b0
rahulsawant811 <rahulsawant811@gmail.com>
2018-01-04 02:26:20 +0530

    Added product model and mongoose to handle an Atlas MongoDB queries

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors')
 
 const app = express();
@@ -8,6 +9,12 @@ const port = process.env.PORT || 5000;
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+//mongodb+srv://node-shop:<PASSWORD>@node-rest-app-g46y1.mongodb.net/test
+mongoose.connect('mongodb://node-shop:'+ process.env.MONGO_ATLAS_PWD +'@node-rest-app-shard-00-00-g46y1.mongodb.net:27017,node-rest-app-shard-00-01-g46y1.mongodb.net:27017,node-rest-app-shard-00-02-g46y1.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-app-shard-0&authSource=admin',
+  {
+    useMongoClient: true
+  }
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({encoded: false}));
@@ -16,6 +23,8 @@ app.use(cors())
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+
 
 app.use((req, res, next) => {
   const error = new Error('Not Found');
